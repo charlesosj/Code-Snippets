583e1db1b9c79250882b0c0d832f9dfd7445f02e
Charles Osei <cosei@hsdyn.com>
2019-10-02 15:17:54 +0100

    File added

---
+++ JS Snippets.js
@@ -0,0 +1,9 @@
+var executionContext = null;
+var formContext= executionContext.getFormContext();
+var recordId =  formContext.data.entity.getId().replace('{', '').replace('}', '');
+
+
+
+Xrm.Utility.alertDialog("error.message");
+
+formContext.ui.setFormNotification("Please add a Line Item in order to Confirm CA and proceed to Drafting Approvals", "ERROR", "NoLineItemNotification");
