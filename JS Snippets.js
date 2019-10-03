8740a374640b471f056d755164bcceede30c11ba
Charles Osei <cosei@hsdyn.com>
2019-10-03 13:31:31 +0100

    stuff added

--- JS Snippets.js
+++ JS Snippets.js
@@ -7,3 +7,5 @@ var recordId =  formContext.data.entity.getId().replace('{', '').replace('}', ''
 Xrm.Utility.alertDialog("error.message");
 
 formContext.ui.setFormNotification("Please add a Line Item in order to Confirm CA and proceed to Drafting Approvals", "ERROR", "NoLineItemNotification");
+
+Xrm.Page.getAttribute('<FIELD_NAME>').getValue()[0].id;)
